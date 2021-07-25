0150c1e2dff5d7f84fa0dfebea6b0c5c3191b212
Mike Johnston <mrbean355@gmail.com>
2021-07-25 15:44:18 +0200

    Prepare for demo

--- app/src/main/java/com/github/mrbean355/android/hilt/MainViewModel.kt
+++ app/src/main/java/com/github/mrbean355/android/hilt/MainViewModel.kt
@@ -4,33 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.mrbean355.android.hilt.data.BeerCache
 import com.github.mrbean355.android.hilt.data.BeerRepositoryImpl
 import com.github.mrbean355.android.hilt.service.Beer
-import com.github.mrbean355.android.hilt.service.BeerService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
-class MainViewModel : ViewModel() {
-    private val repo = BeerRepositoryImpl(createService(), BeerCache, Dispatchers.IO)
+class MainViewModel(
+    private val repo: BeerRepositoryImpl
+) : ViewModel() {
     private val _beers = MutableLiveData<List<Beer>>()
 
     val beers: LiveData<List<Beer>> get() = _beers
+
+    @Suppress("unused") // called by ViewModelProvider
+    constructor() : this(BeerRepositoryImpl())
 
     fun initialise() {
         viewModelScope.launch {
             _beers.value = repo.getBeers().sortedBy { it.name }
         }
-    }
-
-    private fun createService(): BeerService {
-        return Retrofit.Builder()
-            .baseUrl("https://api.punkapi.com/v2/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create()
     }
 }
--- app/src/main/java/com/github/mrbean355/android/hilt/data/BeerRepository.kt
+++ app/src/main/java/com/github/mrbean355/android/hilt/data/BeerRepository.kt
@@ -3,25 +3,33 @@ package com.github.mrbean355.android.hilt.data
 import com.github.mrbean355.android.hilt.service.Beer
 import com.github.mrbean355.android.hilt.service.BeerService
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
-
-interface BeerRepository {
-
-    suspend fun getBeers(): List<Beer>
-
-}
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class BeerRepositoryImpl(
     private val beerService: BeerService,
     private val beerCache: BeerCache,
     private val dispatcher: CoroutineDispatcher
-) : BeerRepository {
+) {
 
     private val mutex = Mutex()
 
-    override suspend fun getBeers(): List<Beer> = withContext(dispatcher) {
+    constructor() : this(
+        beerService = Retrofit.Builder()
+            .baseUrl("https://api.punkapi.com/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(),
+        beerCache = BeerCache,
+        dispatcher = Dispatchers.IO
+    )
+
+    suspend fun getBeers(): List<Beer> = withContext(dispatcher) {
         mutex.withLock {
             beerCache.load()?.let {
                 return@withContext it
